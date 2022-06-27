25d42d35357748edfcdd6a12bad81f52c603ad03
Garywtne <gary.whitney@stiga.com>
2022-06-27 17:44:39 +0100

    remove surplus folders

--- Resources/TIDY UP REPO/calendar.sql
+++
@@ -1,7 +0,0 @@
--- Table: public.calendar
-
-CREATE TABLE IF NOT EXISTS public.calendar
-(
-    "Date" text COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT pk_calendar PRIMARY KEY ("Date")
-)
